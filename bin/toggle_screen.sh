bcf187c7b2f7e4d2a74269f3000bb93403008cbc
Guido Vicino <mail@guidovicino.com>
2024-01-04 21:20:29 +0100

    Fixed a bug in lid toggle script

--- bin/toggle_screen.sh
+++ bin/toggle_screen.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-if grep -q open /proc/acpi/button/lid/LID/state; then
-    swaymsg output <LAPTOP> enable
+
+LAPTOP=eDP-1
+if grep -q open /proc/acpi/button/lid/LID0/state; then
+    swaymsg output $LAPTOP enable
 else
-    swaymsg output <LAPTOP> disable
+    swaymsg output $LAPTOP disable
 fi
